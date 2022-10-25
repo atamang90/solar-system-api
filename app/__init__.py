e8afa3e6eb58ed5748cc66793fc833845e2ad89d
Anjula Tamang <parkhrinj@gmail.com>
2022-10-25 15:16:41 -0400

    did wave 02

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
     
-    from .routes import planets_bp
+    from .planets import planets_bp
     app.register_blueprint(planets_bp)
 
     
--- app/planets.py
+++ app/planets.py
@@ -1,33 +1,52 @@
-# from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
+
+class Planet:
+    def __init__(self,id,name,description,distance):
+        self.id=id
+        self.name=name
+        self.description=description
+        self.distance=distance
+
+PLANETS = [
+           Planet(1,"Mercury","first from the sun",35000000),
+           Planet(2,"Venus","second from the sun",67000000),
+           Planet(3,"Earth","third from the sun",93000000),
+           Planet(4,"Mars","fourth from the sun",142000000),
+           Planet(5,"Jupiter","fifth from the sun",484000000),
+           Planet(6,"Saturn","sixth from the sun",889000000),
+           Planet(7,"Uranus","seventh from the sun",1790000000),
+           Planet(8,"Neptune","eighth from the sun",2880000000)]
+
+planets_bp = Blueprint("planets_bp", __name__, url_prefix ='/planets')
 
 
-# class Planet:
-#     def __init__(self,id,name,description,distance):
-#         self.id=id
-#         self.name=name
-#         self.description=description
-#         self.distance=distance
-
-# PLANETS = [
-#            Planet(1,"Mercury","first from the sun",35000000),
-#            Planet(2,"Venus","second from the sun",67000000),
-#            Planet(3,"Earth","third from the sun",93000000),
-#            Planet(4,"Mars","fourth from the sun",142000000),
-#            Planet(5,"Jupiter","fifth from the sun",484000000 ),
-#            Planet(6,"Saturn","sixth from the sun",889000000),
-#            Planet(7,"Uranus","seventh from the sun",1790000000),
-#            Planet(8,"Neptune","eighth from the sun",2880000000)]
-
-# planets_bp = Blueprint("planets_bp", __name__, url_prefix ='/planets')
+@planets_bp.route('', methods = ['GET'])
+def get_all_planets():
+    planet_response =[vars(planet) for planet in PLANETS]
+    return jsonify(planet_response)
 
 
-# @planets_bp.route('', methods = ['GET'])
-# def get_all_planets():
-#     planet_response=[]
-#     for planet in PLANETS:
-#         planet_response.append({"id":planet.id,
-#                                 "name":planet.name,
-#                                 "description":planet.description,
-#                                 "distance":planet.distance,
-#                                 })
-#     return jsonify(planet_response)
+@planets_bp.route('/55', methods = ['GET'])
+def get_one_planet(id):
+    planet = validate_planet(id)
+    return planet
+
+def validate_planet(id):
+    try:
+        planet_id = int(id)
+    except ValueError:
+        return {"message": "Invalid planet id" },400
+    
+    for planet in PLANETS:
+        if planet.id == planet_id:
+            return vars(planet)
+    
+    abort(make_response(jsonify(description = "Not Found"), 404))
+    
+    
+
+
+
+
+    
+
