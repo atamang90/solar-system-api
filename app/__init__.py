2b7338efb78f1fa558b76b64579c9908a975e25c
Anjula Tamang <parkhrinj@gmail.com>
2022-11-05 16:33:42 -0400

    none

--- app/__init__.py
+++ app/__init__.py
@@ -12,10 +12,11 @@ def create_app(test_config=None):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
 
-    from app.models.model_planet import Planet
+    
 
     db.init_app(app)
     migrate.init_app(app, db)
+    from app.models.model_planet import Planet
 
     from .planets import planets_bp
     app.register_blueprint(planets_bp)
